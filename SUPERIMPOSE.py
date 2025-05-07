d0c8cc55c8e61a1407cd84b39d481158db8946a8
SamYT-dur <152638191+SamYT-dur@users.noreply.github.com>
2025-05-07 13:49:10 +0100

    Update SUPERIMPOSE.py

--- SUPERIMPOSE.py
+++ SUPERIMPOSE.py
@@ -14,14 +14,14 @@ def main(refcode):
     csd_reader = EntryReader('CSD') #find spacegroup
     entry = csd_reader.entry(refcode)
     spg = entry.crystal.spacegroup_symbol
-    #bad_list = ['P31c', 'R3m', 'R3', 'R3c', 'R-3m'] #sometimes fail when reading into pymatgen: Some occupancies ([1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) sum to > 1! If they are within the occupancy_tolerance, they will be rescaled. The current occupancy_tolerance is set to: 1.0 Species occupancies sum to more than 1!
+    bad_list = ['P31c', 'R3m', 'R3', 'R3c', 'R-3m'] # these sometimes fail when reading into pymatgen: Some occupancies ([1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) sum to > 1! If they are within the occupancy_tolerance, they will be rescaled. The current occupancy_tolerance is set to: 1.0 Species occupancies sum to more than 1!
     try:
-    #    if spg in bad_list: #fixes some of the issues by reading into pyxtal first. i think the problem is multiple atoms on one site
-        pyx_struc = pyxtal()
-        pyx_struc.from_seed('OutputCIFs/CSD/{}.cif'.format(refcode))
-        low_struc = pyx_struc.to_pymatgen()
-        #else: #this is how it should run without errors. All other space groups seem fine for now. not definitive
-            #low_struc = Structure.from_file('OutputCIFs/CSD/{}.cif'.format(refcode), merge_tol=0.01) #can also compare to simplified cifs using 'MasterOutputs/Top/{}_top.cif' as string.
+        if spg in bad_list: #fixes some of the issues by reading into pyxtal first. i think the problem is multiple atoms on one site
+            pyx_struc = pyxtal()
+            pyx_struc.from_seed('OutputCIFs/CSD/{}.cif'.format(refcode))
+            low_struc = pyx_struc.to_pymatgen()
+        else: #this is how it should run without errors. All other space groups seem fine for now. not definitive
+            low_struc = Structure.from_file('OutputCIFs/CSD/{}.cif'.format(refcode), merge_tol=0.01) #can also compare to simplified cifs using 'MasterOutputs/Top/{}_top.cif' as string.
         high_struc = Structure.from_file('OutputCIFs/Output/{}_top_findsym.cif'.format(refcode))
         new_struc = Structure.from_sites(low_struc.sites)
     except Exception as error:
@@ -64,7 +64,7 @@ def main(refcode):
         coords2 = coords2.transpose()
         coords3 = np.matmul(trans_matrix2, coords2)
         coords4 = coords3 + origin_shift
-        coords5 = [float(coords4[0]), float(coords4[1]), float(coords4[2])]
+        coords5 = [float(coords4[0].item()), float(coords4[1].item()), float(coords4[2].item())]
         high_coords3.append(coords5)
 
     for species, coords in zip(high_species, high_coords3):
@@ -74,9 +74,11 @@ def main(refcode):
     a.write_file('OutputCIFs/Superimpose/{}_comp.cif'.format(refcode))
 
 
+
+
 if __name__ == '__main__':
     with open('compcif_errors.txt', 'w+') as outfile:
-        for file in os.listdir('OutputCIFs/Output'):
+        for file in os.listdir('OutputCIFs/Superimpose/{}_comp.cif'.format(refcode)):
             filename = os.fsdecode(file)
             refcode = filename.strip('_top_findsym.cif')
            #try:
